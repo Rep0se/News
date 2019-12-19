7bdd8440a6601d91d7b5c4c474a935db0aeed2d7
Alexander Sundiev <alexander.sundiev@live.ca>
2019-12-19 18:28:20 -0500

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Alexander Sundiev on 2019-12-19.
+//  Copyright © 2019 Alexander Sundiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
